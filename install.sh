e9c17c3fa6ad9aaf1038b0743a525d6f7faf3e2c
Rafael Vargas <rafael.vargas.emp@gmail.com>
2023-03-28 20:58:41 -0300

    update script

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # --------------------- APPS ------------------#
-
+sudo pacman -Syu 
 sudo pacman -S git;
-git clone https://aur.archlinux.org/snapd.git && cd snapd && makepkg -si && cd ~
 
+sudo pacman -S snapd;
 sudo systemctl enable --now snapd.socket;
-sudo pacman -S yakuake;
-cd Downloads;
+sudo ln -s /var/lib/snapd/snap /snap;
 
-sudo wget -c https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip && cd ~;
+sudo pacman -S yakuake;
 sudo snap install code --classic;
 sudo snap install beekeeper-studio;
 sudo snap install spotify;
@@ -23,7 +22,6 @@ mv aulas /home/$USER/Documents
 cd ~;
 mv zshrc .zshrc;
 cd ~;
+sudo pacman -S nodejs npm 
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
-
-sudo pacman -Syu && sudo reboot -f
+sudo reboot -f
