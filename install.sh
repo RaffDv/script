38e1a020c09e9cee06957d1d95ba0ff538a0c6c2
Rafael Vargas <rafael.vargas.emp@gmail.com>
2023-03-28 20:59:32 -0300

    script update

--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-# --------------------- APPS ------------------#
+# --------------------- tools ------------------#
 sudo pacman -Syu 
 sudo pacman -S git;
-
 sudo pacman -S snapd;
 sudo systemctl enable --now snapd.socket;
 sudo ln -s /var/lib/snapd/snap /snap;
+# --------------------- git conf ------------------#
 
-sudo pacman -S yakuake;
-sudo snap install code --classic;
-sudo snap install beekeeper-studio;
+git config --global user.email "rafael.vargas.emp@gmail.com";
+git config --global user.name "Rafael Vargas"  ;
+# --------------------- utilitaries ------------------#
+
 sudo snap install spotify;
 sudo pacman -S opera;
 sudo pacman -S steam;
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)";
+
+# <----------------- config archives  ---------------- #
+
 cd ~/Downloads;
 git clone https://github.com/RaffDv/script.git;
 cd script;
@@ -21,7 +24,19 @@ mv zshrc /home/$USER;
 mv aulas /home/$USER/Documents
 cd ~;
 mv zshrc .zshrc;
-cd ~;
+
+# <----------------- code ---------------- #
+
+sudo snap install code --classic;
+sudo snap install beekeeper-studio;
 sudo pacman -S nodejs npm 
+sudo pacman -S yakuake;
+
+# <----------------- zsh ---------------- #
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)";
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git;
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc;
+
+# <----------------- end ---------------- #
 
 sudo reboot -f
