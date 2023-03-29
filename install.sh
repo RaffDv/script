87760a323adc3a996c7123e29ea9a316ba3e6b22
Rafael Vargas <rafael.vargas.emp@gmail.com>
2023-03-29 11:09:02 -0300

    script update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # --------------------- tools ------------------#
-sudo pacman -Syu 
+sudo pacman -Syu;
 sudo pacman -S git;
 sudo pacman -S snapd;
 sudo systemctl enable --now snapd.socket;
@@ -21,15 +21,17 @@ cd ~/Downloads;
 git clone https://github.com/RaffDv/script.git;
 cd script;
 mv zshrc /home/$USER;
-mv aulas /home/$USER/Documents
+mv aulas /home/$USER/Documents;
 cd ~;
 mv zshrc .zshrc;
-
+cd ~/Downloads/Script;
+mv raffAvatar.png /home/$USER/Images/;
+cd .. && rm -rf Script;
 # <----------------- code ---------------- #
 
 sudo snap install code --classic;
 sudo snap install beekeeper-studio;
-sudo pacman -S nodejs npm 
+sudo pacman -S nodejs npm;
 sudo pacman -S yakuake;
 
 # <----------------- zsh ---------------- #
